a3c6793006015e6afc021082b1443734980fbf54
Nukdown <belchior.emanoel@gmail.com>
2019-05-13 11:59:39 -0300

    feat(#4) Create method for login with token

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const app = express()
 const WebProxy = require('./src/webProxy.js')
 const { PORT } = process.env
 
-app.get('/', (req, res) => {
+app.get('/', verifyJWT, (req, res, next) => {
     var options = {
         hostname: 'hello',
         port: 8000,
@@ -17,6 +18,21 @@ app.get('/', (req, res) => {
     var web = new WebProxy(options)
     web.proxy(req, res)
 })
+
+app.post('/login', (req, res, next) => {
+  //this user need be dynamic
+  const user ={
+    id: "1",
+    username: 'username'
+  }
+    var token = jwt.sign({user: user}, 'secretarg', {
+      expiresIn: 300 // expires in 5min
+    });
+    res.status(200).send({ auth: true, token: token });
+
+  res.status(500).send('Login inválido!');
+})
+
 
 app.listen(PORT, () => {
     console.log('Server running on port ' + PORT)
