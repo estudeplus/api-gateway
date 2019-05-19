aaa856dfb4df62ed1fa0399ac14de4b442f60d8e
matheusbsilva <matheusbattista@hotmail.com>
2019-05-19 11:38:20 -0300

    feat(#5): use service class on WebProxy class

--- src/service.js
+++ src/service.js
@@ -2,6 +2,17 @@
 
 class Service {
     constructor(options){
+        /* 
+            Example of options:
+            options = {
+                hostname: 'localhost',
+                port: 80,
+                path: '/',
+                method: 'GET',
+                headers: {}
+            }
+            docs: https://nodejs.org/api/http.html#http_http_request_options_callback
+        */
         this._options = options
     }
 
