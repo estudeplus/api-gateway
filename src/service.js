20cdf6bf4d54cf1ab661d65b518e7be7f26540bb
matheusbsilva <matheusbattista@hotmail.com>
2019-05-19 11:53:54 -0300

    feat(#5): validation of options args on Service class

--- src/service.js
+++ src/service.js
@@ -13,6 +13,21 @@ class Service {
             }
             docs: https://nodejs.org/api/http.html#http_http_request_options_callback
         */
+
+        if(typeof(options) != 'object'){
+            return new Error("args must be an object") 
+        }
+
+        const requiredKeys = [ 'hostname', 'port', 'path', 'method' ]
+
+        const optionsKeys = Object.keys(options)
+
+        for(key in requiredKeys) {
+            if(!key in optionsKeys) {
+                return new Error(`${key} is missing on args`)
+            }
+        }
+
         this._options = options
     }
 
